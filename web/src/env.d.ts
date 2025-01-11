d6841dfcf822d256ab1632103493096a191cf4c2
Jositngl <jostinjmhernandez@gmail.com>
2025-01-11 14:59:26 +0100

    Agregar definiciones de tipos para las variables de entorno de Sanity

---
+++ web/src/env.d.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="@sanity/astro/module" />
+
+interface ImportMetaEnv {
+    readonly PUBLIC_SANITY_PROJECT_ID: string;
+    readonly PUBLIC_SANITY_DATASET: string;
+    readonly PUBLIC_SANITY_API_VERSION: string;
+}

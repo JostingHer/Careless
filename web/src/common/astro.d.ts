1a4782948939c05f07f29ecc30d250e78341d316
JostingHer <jostingjmhernandez@gmail.com>
2025-04-08 21:12:37 +0200

    selectorLanguage

--- web/src/common/astro.d.ts
+++ web/src/common/astro.d.ts
@@ -1,5 +1,15 @@
+import type { LANGUAGE } from "@global-shared/translations/domain/languages";
+import type { CollectionKey } from "astro:content";
+
 export type Data<T> = {
     id: string;
     data: T;
     collection: string;
+};
+
+export type SharedContext = {
+    id: string;
+    language: LANGUAGE;
+    collection: CollectionKey;
+    ref: string;
 };

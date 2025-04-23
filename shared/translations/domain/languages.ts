bbe77d483b9889c390935ba23bdf9adf7475d81d
JostingHer <jostingjmhernandez@gmail.com>
2025-04-23 22:34:50 +0200

    Eliminar el idioma francés de las traducciones y limpiar el manejo de secciones en HomeBlogPage.

--- shared/translations/domain/languages.ts
+++ shared/translations/domain/languages.ts
@@ -1,7 +1,6 @@
 const enum LANGUAGE {
     ES = "es",
     EN = "en",
-    FR = "fr",
 }
 const DEFAULT_LANGUAGE: LANGUAGE = LANGUAGE.ES;
 
@@ -11,5 +10,4 @@ export const LIMIT_CHARACTERS_IN_TRANSLATION = 9200;
 export const Languages = [
     { title: "Español", value: LANGUAGE.ES },
     { title: "Inglés", value: LANGUAGE.EN },
-    { title: "Francés", value: LANGUAGE.FR },
 ];

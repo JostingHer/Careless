4d4a02b9006cf92f9736d3fb3b56fa4246d4ac23
JostingHer <jostingjmhernandez@gmail.com>
2025-05-31 12:28:46 +0200

    change server

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -16,7 +16,7 @@ import dotenv from "dotenv";
 dotenv.config();
 // https://astro.build/config
 export default defineConfig({
-    output: "server", // default
+    output: "server", //  Server-side rendering
     env: {
         schema: {
             ENV: envField.string({

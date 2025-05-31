1ce5811f4fa9ff130fac5276311d2a07579ebc0c
JostingHer <jostingjmhernandez@gmail.com>
2025-05-31 15:23:57 +0200

    structure shop

--- web/src/content/config.ts
+++ web/src/content/config.ts
@@ -2,10 +2,12 @@ import { authorInSanity } from "@/contexts/Blog/infraestructure/sanity-author-bl
 import { categoryInSanity } from "@/contexts/Blog/infraestructure/sanity-category-blog-collection";
 import { homeBlogInSanity } from "@/contexts/Blog/infraestructure/sanity-home-blog-collection";
 import { postInSanity } from "@/contexts/Blog/infraestructure/sanity-post-blog-collection";
+import { productInPrestashop } from "@/contexts/Shop/infraestructure/ps-product-shop-collection";
 
 export const collections = {
     HomeBlog: homeBlogInSanity,
     Post: postInSanity,
     Category: categoryInSanity,
     Author: authorInSanity,
+    Product: productInPrestashop,
 };

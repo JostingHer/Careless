f108a173f911f5d67086f4f5a5dd1f37c2619986
JostingHer <jostingjmhernandez@gmail.com>
2025-06-11 23:44:17 +0200

    Agregar lógica de registro de usuario: implementar registerAction y crear componente RegisterForm. Actualizar AuthShopPage y registrar la ruta en register.astro. Eliminar código no utilizado en test2.astro.

--- web/src/actions/index.ts
+++ web/src/actions/index.ts
@@ -1,5 +1,7 @@
+import { registerAction } from "@/features/auth/infraestructure/register-action";
 import { sendDataAction } from "../features/getting-leads/infrastructure/send-data-action";
 
 export const server = {
     sendData: sendDataAction,
+    registerUser: registerAction,
 };
